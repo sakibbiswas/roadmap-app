4d7468a5a708db3b735121a53e5115d994c3b30b
Sakib Biswas <sakibsakib99880@gmail.com>
2025-07-19 00:00:45 +0600

    fix add

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,15 +1,15 @@
 "use strict";
-// import app from "./app";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-// backend/src/server.ts
 const app_1 = __importDefault(require("./app"));
-//  Don't use app.listen()
-//  Instead, export the app for Vercel
-exports.default = app_1.default;
+const PORT = process.env.PORT || 5000;
+app_1.default.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+// // backend/src/server.ts
+// import app from "./app";
+// //  Don't use app.listen()
+// //  Instead, export the app for Vercel
+// export default app;
